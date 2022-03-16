43bc041a94183dfb198d35b3592fabd09f6f6942
Savu Roxana <savuroxanaflorina20@yahoo.com>
2022-03-16 22:11:05 +0200

    Commit 1

--- nextjs-course-code/next.config.js
+++ nextjs-course-code/next.config.js
@@ -1,23 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: 'maximilian',
-        mongodb_password: '2YkcXq43KyPk0vqp',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site-dev',
-      },
-    };
-  }
-
-  return {
-    env: {
-      mongodb_username: 'maximilian',
-      mongodb_password: '2YkcXq43KyPk0vqp',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'my-site',
-    },
-  };
-};
+module.exports = nextConfig
